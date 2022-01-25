6828869714d1b78c6c8ff01826a178d4e47bb55e
ShotaKinoshita <onque.unchain@gmail.com>
2022-01-25 08:11:36 +0000

    [debug]ルーティング調整・viewファイルyield

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,7 @@ Rails.application.configure do
   config.hosts << "49e69874be6d4ef3a0718e9b15430b98.vfs.cloud9.us-east-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "d59b8a3b6d3c4113a75650708c6c8c6b.vfs.cloud9.us-east-1.amazonaws.com"
+
 end
