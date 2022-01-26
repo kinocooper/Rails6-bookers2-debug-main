071e087471d33d8aa9e672936234c039904155a3
ShotaKinoshita <onque.unchain@gmail.com>
2022-01-26 02:12:27 +0000

    [debug]ログイン外ユーザの本の編集制限

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,11 @@
 class BooksController < ApplicationController
 
+  before_action :ensure_correct_user, only:[:edit]
+
+
   def show
     @book = Book.find(params[:id])
+    @user = current_user
   end
 
   def index
@@ -21,7 +25,6 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
   end
 
   def update
@@ -33,9 +36,9 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
@@ -44,4 +47,12 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title,:body)
   end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless current_user == @book.user
+      redirect_to books_path
+    end
+  end
+
 end
